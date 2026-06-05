2de6897cfa8f2859e9ab2e3855fa372b374cd3e7
Tehillah M <tehillahmuchato@gmail.com>
2026-06-05 17:21:01 +0100

    Update HeroSlideshow.tsx
    changed the transition
    framer motion used "wait" which waited for image to end instead of smoothly transitioning
    images should now run simulataneously

--- components/HeroSlideshow.tsx
+++ components/HeroSlideshow.tsx
@@ -25,11 +25,11 @@ export default function HeroSlideshow() {
   return (
     <section className="relative h-screen w-full overflow-hidden">
       {/* Slides */}
-      <AnimatePresence mode="wait">
+      <AnimatePresence>
         <motion.div
           key={current}
-          initial={{ opacity: 0, scale: 1.04 }}
-          animate={{ opacity: 1, scale: 1 }}
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
           exit={{ opacity: 0 }}
           transition={{ duration: 1.4, ease: "easeInOut" }}
           className="absolute inset-0"
